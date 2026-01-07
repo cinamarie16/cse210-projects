7c9d18001e76c6a1f6097a8d0e45fdd81b9282d7
cinamarie16 <cinamarie16@gmail.com>
2026-01-07 14:24:30 -0700

    week 1 exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        //Console.Write("What is the magic number? ");
+        //int magicNumber = int.Parse(Console.ReadLine());
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+
+        int numberGuess = -1;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            numberGuess = int.Parse(Console.ReadLine());
+
+            if (numberGuess == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+            else if (numberGuess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+        } while (numberGuess != magicNumber);
+
     }
 }
