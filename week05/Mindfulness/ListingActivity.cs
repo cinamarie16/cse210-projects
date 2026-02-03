417748e67079689fb30e79192a0ad0386814d5d7
cinamarie16 <cinamarie16@gmail.com>
2026-02-02 18:24:50 -0700

    week 5 mindfulness project stubbing

---
+++ week05/Mindfulness/ListingActivity.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+
+public class ListingActivity : Activity
+{
+    private int _count;
+    private List<string> _prompts;
+
+    public ListingActivity()
+    {
+        
+    }
+
+    public void Run()
+    {
+        
+    }
+
+    private void GetRandomPrompt()
+    {
+        
+    }
+
+    public List<string> GetListFromUser()
+    {
+        return [""];
+    }
+}
---
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+
+public class ReflectingActivity : Activity
+{
+    private List<string> _prompts;
+    private List<string> _questions;
+
+    public ReflectingActivity()
+    {
+        
+    }
+
+    public void Run()
+    {
+        
+    }
+
+    private string GetRandomPrompt()
+    {
+        return "";
+    }
+
+    private string GetRandomQuestion()
+    {
+        return "";
+    }
+
+    public void DisplayPrompt()
+    {
+        
+    }
+
+    public void DisplayQuestion()
+    {
+        
+    }
+}
